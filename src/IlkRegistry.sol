16b90106a632609b23c72960d0c1e91a33187558
Brian McMichael <brian@brianmcmichael.com>
2021-03-29 12:14:05 -0400

    fiddling

--- src/IlkRegistry.sol
+++ src/IlkRegistry.sol
@@ -239,8 +239,8 @@ contract IlkRegistry {
 
     // Authed edit function
     function file(bytes32 ilk, bytes32 what, uint256 data) external auth {
-        if      (what == "class") { require (data <= uint96(-1)); ilkData[ilk].class = uint96(data); }
-        else if (what == "dec")   { require (data <= uint8(-1));  ilkData[ilk].dec   = uint8(data); }
+        if      (what == "class") { require(data <= uint96(-1)); ilkData[ilk].class = uint96(data); }
+        else if (what == "dec")   { require(data <= uint8(-1));  ilkData[ilk].dec   = uint8(data); }
         else revert("IlkRegistry/file-unrecognized-param-uint256");
     }
 
