2aa6f972cfa35474492cfa5da61f77a00ef3c6a4
Nazzareno Massari <nas@naszam.co>
2022-04-26 14:32:45 +0100

    update `ilks2ascii.sh`

--- scripts/ilks2ascii.sh
+++ scripts/ilks2ascii.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
-### ilks2ascii -- list ilks from ilk-registry in ascii
-### Usage: ./ilks2ascii <ilk-registry-address>
+#!/usr/bin/env bash
+### ilks2ascii -- list ilks from the IlkRegistry in ascii
+### Usage: ./ilks2ascii.sh
 
-ILKS=$(seth call "$1" "list()(bytes32[])")
+set -e
 
-for ilk in $(echo "$ILKS" | sed "s/,/ /g")
+[[ "$ETH_RPC_URL" ]] || { echo "Please set a ETH_RPC_URL"; exit 1; }
+
+CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
+
+ILK_REGISTRY=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "ILK_REGISTRY")")")
+
+LIST=$(seth call "$ILK_REGISTRY" 'list()(bytes32[])')
+
+echo -e "Network: $(seth chain)"
+for ilk in $(echo -e "$LIST" | sed "s/,/ /g")
 do
-	seth --to-ascii "$ilk"
-
+    seth --to-ascii "$ilk"
 done
