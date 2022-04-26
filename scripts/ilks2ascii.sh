5338bfbf6aa8a9ff17e3589e1e8d673e36347f9c
Nazzareno Massari <nas@naszam.co>
2022-04-26 15:35:02 +0100

    address seth gas price error

--- scripts/ilks2ascii.sh
+++ scripts/ilks2ascii.sh
@@ -6,6 +6,11 @@ set -e
 
 [[ "$ETH_RPC_URL" ]] || { echo "Please set a ETH_RPC_URL"; exit 1; }
 
+### Override maxFeePerGas to avoid spikes
+baseFee=$(seth basefee)
+[[ -n "$ETH_GAS_PRICE" ]] && ethGasPriceLtBaseFee=$(echo "$ETH_GAS_PRICE < $baseFee" | bc)
+[[ "$ethGasPriceLtBaseFee" == 1 ]] && export "ETH_GAS_PRICE=$(echo "$baseFee * 3" | bc)"
+
 CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
 
 ILK_REGISTRY=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "ILK_REGISTRY")")")
