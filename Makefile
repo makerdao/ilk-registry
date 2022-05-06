f076565c0531fd458cfdca2740cca7c53603d2b2
Nazzareno Massari <nas@naszam.co>
2022-05-06 23:11:53 +0100

    Update `ilks2ascii.sh` (#28)

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ all    :; DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=1000000 dapp --use solc
 clean  :; dapp clean
 test   :; DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=1000000 dapp --use solc:0.6.12 test -v
 deploy-mainnet :; ./scripts/deploy-mainnet.sh
-deploy-kovan :; ./scripts/deploy-kovan.sh
+deploy-kovan   :; ./scripts/deploy-kovan.sh
+list           :; ./scripts/ilks2ascii.sh
--- scripts/ilks2ascii.sh
+++ scripts/ilks2ascii.sh
@@ -1,11 +1,24 @@
-#!/bin/bash
-### ilks2ascii -- list ilks from ilk-registry in ascii
-### Usage: ./ilks2ascii <ilk-registry-address>
+#!/usr/bin/env bash
+### ilks2ascii -- list ilks from the IlkRegistry in ascii
+### Usage: ./ilks2ascii.sh
 
-ILKS=$(seth call "$1" "list()(bytes32[])")
+set -e
 
-for ilk in $(echo "$ILKS" | sed "s/,/ /g")
+[[ "$ETH_RPC_URL" ]] || { echo "Please set a ETH_RPC_URL"; exit 1; }
+
+### Override maxFeePerGas to avoid spikes
+baseFee=$(seth basefee)
+[[ -n "$ETH_GAS_PRICE" ]] && ethGasPriceLtBaseFee=$(echo "$ETH_GAS_PRICE < $baseFee" | bc)
+[[ "$ethGasPriceLtBaseFee" == 1 ]] && export "ETH_GAS_PRICE=$(echo "$baseFee * 3" | bc)"
+
+CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
+
+ILK_REGISTRY=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "ILK_REGISTRY")")")
+
+LIST=$(seth call "$ILK_REGISTRY" 'list()(bytes32[])')
+
+echo -e "Network: $(seth chain)"
+for ilk in $(echo -e "$LIST" | sed "s/,/ /g")
 do
-	seth --to-ascii "$ilk"
-
+    seth --to-ascii "$ilk"
 done
